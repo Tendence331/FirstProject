3cfe25204dde3f55a2cf4414ac5e488aae27648e
Vyacheslav <lance110741@gmail.com>
2025-01-21 15:34:07 +0300

    Add code with first.cpp

--- first.cpp
+++ first.cpp
@@ -1,1 +1,28 @@
-first message
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float x, y;
+    int ech;
+    cout << "Введите два числа: " << endl;
+    cin >> x;
+    cin >> y;
+    cout << "Выберите действие: " << endl;
+    cout << "1. Сложение" << endl;
+    cout << "2. Вычитание" << endl;
+    cin >> ech;
+
+    switch (ech)
+    {
+    case 1:
+        cout << "Сумма " << x << " и " << y << " = " << x + y << endl;
+        break;
+    case 2:
+        cout << "Вычитание " << x << " и " << y << " = " << x + y << endl;
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
